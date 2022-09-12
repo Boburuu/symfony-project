19fad749fae9d603c2872fa09f481dd8629eca1e
Boburuu <camille.disera.30@gmail.com>
2022-09-12 08:43:09 +0200

    feat: eferezez
    eezrrzerzerzzrz

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -5,13 +5,13 @@ namespace App\Form;
 use App\Entity\Article;
 use App\Entity\Categorie;
 use Doctrine\ORM\EntityRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ArticleType extends AbstractType
 {
@@ -22,30 +22,36 @@ class ArticleType extends AbstractType
                 'label' => 'Titre:',
                 'required' => true
             ])
-            ->add('content', TextareaType::class, [
-                'label' => 'Contenu:',
-                'required' => true
-            ])
 
-            ->add('categories',EntityType::class, [
+            ->add('categories', EntityType::class, [
                 'label' => 'Categories:',
                 'class' => Categorie::class,
                 'choice_label' => 'titre',
                 'multiple' => true,
-                'by_reference' => false, 
+                'by_reference' => false,
                 # Le $er abrevation de EntityRepository(attribut) en gros ont instencie Entity dans $er pour la reutiliser dans la fonction 
-                "query_builder" => function (EntityRepository $er){
+                "query_builder" => function (EntityRepository $er) {
                     #Creer une requete sql en php, createQueryBuilder()-> permet de créer une nouvelle requête,
                     return $er->createQueryBuilder('c')
-                    ->andWhere('c.enable = true')
-                    ->orderBy('c.titre', 'ASC');
+                        ->andWhere('c.enable = true')
+                        ->orderBy('c.titre', 'ASC');
                 },
-               
+            ])
+            // ->add('save', SubmitType::class, [
+            //     'label' => 'CRÉER',
+            // ]);
 
-            ] );
-        // ->add('save', SubmitType::class, [
-        //     'label' => 'CRÉER',
-        // ]);
+            ->add('imageFile', VichImageType::class, [
+                'required' => false,
+                'download_uri' => false,
+                'image_uri' => true,
+                'label' => 'Image',
+            ])
+
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu:',
+                'required' => true
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
